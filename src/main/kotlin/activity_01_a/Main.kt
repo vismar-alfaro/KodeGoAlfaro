003902dd563d435c5195bf6f0ea9d2a95922f820
Vismar Jay Alfaro <zyrenmisaki@gmail.com>
2022-10-08 09:14:51 +0800

    added package activity_01_1 added Main

--- src/main/kotlin/activity_01_a/Main.kt
+++ src/main/kotlin/activity_01_a/Main.kt
@@ -1,4 +1,23 @@
 package activity_01_a
 
-class Main {
+import java.time.LocalDate
+import java.time.Month
+import java.time.format.DateTimeFormatter
+import java.util.Date
+
+fun main (){
+
+    var programEnrolled: String? = null
+    var classStartDate = LocalDate.parse("2022-10-06")
+    var dateFormat = DateTimeFormatter.ofPattern("MM-dd-yyy")
+    var formattedWorkStartDate = classStartDate.format(dateFormat)
+    var firstName: String? = null
+    var lastName: String? = null
+    var middleName: String? = null
+
+    var emailAddress: String? = null
+
+
+
+
 }
