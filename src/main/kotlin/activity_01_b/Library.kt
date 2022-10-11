e85c4eb28df6c6b79cfe8dd20ada7c58bf0cb898
Vismar Jay Alfaro <zyrenmisaki@gmail.com>
2022-10-11 19:09:35 +0800

    added package activity_01_b added Library

--- src/main/kotlin/activity_01_b/Library.kt
+++ src/main/kotlin/activity_01_b/Library.kt
@@ -3,7 +3,9 @@ package activity_01_b
 fun main() {
 
     //Books in the Library if can be borrowed
-    var book1: Boolean = false
-
-
+    var rizal: Boolean = true
+    var discreteMath: Boolean = true
+    var modernStat: Boolean = true
+    var purposiveCom: Boolean = true
+    var artApp: Boolean = true
 }
