d26e3055997feb40a6a8ee857845d3148d572521
Vismar Jay Alfaro <zyrenmisaki@gmail.com>
2022-10-11 19:28:30 +0800

    added package activity_01_c added Grocery

---
+++ src/main/kotlin/activity_1_c/Grocery.kt
@@ -0,0 +1,16 @@
+package activity_1_c
+
+fun main() {
+
+    //Items in the grocery
+    var groceryProduct1: String? = null
+    var groceryProduct2: String? = null
+    var groceryProduct3: String? = null
+
+    //Item Category
+    var junkFood: Boolean = false
+    var fruit: Boolean = false
+    var cannedGood: Boolean = false
+
+
+}
