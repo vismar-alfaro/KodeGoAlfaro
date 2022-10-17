64aa0f75488937fcfa95cc97479d8285c8af83b7
Vismar Jay Alfaro <zyrenmisaki@gmail.com>
2022-10-17 19:41:51 +0800

    updated activity01_b

--- src/main/kotlin/activity_01_b/Library.kt
+++ src/main/kotlin/activity_01_b/Library.kt
@@ -8,4 +8,11 @@ fun main() {
     var modernStat: Boolean = true
     var purposiveCom: Boolean = true
     var artApp: Boolean = true
+
+    //Book Characteristics
+    val author: String? = null
+    val yearPublished: Int? = null
+    val publisher: String? = null
+    val edition: String? = null
+    val placePublished: String? = null
 }
