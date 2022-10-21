07fb5353c5b03d63305bf1cef687c98d983e6f12
Vismar Jay Alfaro <zyrenmisaki@gmail.com>
2022-10-21 19:21:45 +0800

    initial commit

---
+++ src/main/kotlin/activity_01_i/LibraryBorrow.kt
@@ -0,0 +1,11 @@
+package activity_01_i
+
+//Using Activity 01 - B, Implement a process where someone can borrow a book.
+//
+//User: Librarian
+//
+//Goal: Keep track book borrowed, who borrowed and how long it can be borrowed.
+//
+//Scope :
+//
+//Data Structures
---
+++ src/main/kotlin/activity_01_j/GroceryCart.kt
@@ -0,0 +1,13 @@
+package activity_01_j
+
+//Using Activity 01 - C
+//
+//Implement a process where items are added to the grocery cart.
+//
+//User : Cashier
+//
+//Goal : List of Items bought, how many items were bought and total cost.
+//
+//Scope :
+//
+//Data Structures
