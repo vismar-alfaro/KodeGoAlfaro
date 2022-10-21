345b1fb60e6680d9f9ab1a855df2d60d1dea4b8c
Vismar Jay Alfaro <zyrenmisaki@gmail.com>
2022-10-21 19:15:42 +0800

    initial commit

---
+++ src/main/kotlin/activity_01_h/PrimeNumbers.kt
@@ -0,0 +1,13 @@
+package activity_01_h
+
+//Create an application that will accept 2 Integers.
+//
+//It will identify the lower value and bigger value of the two inputs.
+//
+//Then, your application will print all the prime numbers starting from the lower number up to the largest number.
+//
+//Scope :
+//
+//String
+//
+//Loops
