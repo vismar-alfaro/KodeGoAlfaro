3816f57f2370ba0516fead10d487373594577ad5
Vismar Jay Alfaro <zyrenmisaki@gmail.com>
2022-10-22 12:00:30 +0800

    added package activity_01_i added LibraryBorrow.kt

--- src/main/kotlin/activity_01_i/LibraryBorrow.kt
+++ src/main/kotlin/activity_01_i/LibraryBorrow.kt
@@ -1,11 +1,58 @@
 package activity_01_i
 
 //Using Activity 01 - B, Implement a process where someone can borrow a book.
-//
 //User: Librarian
-//
 //Goal: Keep track book borrowed, who borrowed and how long it can be borrowed.
-//
 //Scope :
-//
 //Data Structures
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
+
+fun main() {
+
+    val availableBooks = ArrayList<String>()
+    var booksBorrowed: ArrayList<String> = ArrayList()
+    var hashBorrower: HashMap<String,String> = HashMap<String,String>(3)
+
+    availableBooks.add("Life and Works of Rizal")
+    availableBooks.add("Programming 1")
+    availableBooks.add("Data Structures")
+    availableBooks.add("Mathematics 1")
+    availableBooks.add("Purposive Communication")
+    availableBooks.add("Statistics")
+    availableBooks.add("Logic and Ethics")
+
+    for(book in availableBooks){
+        logger.info {"Books Available: $book" }
+    }
+
+    logger.info{"Enter Book Title to be Borrowed: "}
+    val enteredBook =  readLine().toString()
+    if(enteredBook in availableBooks){
+        booksBorrowed.add(enteredBook)
+    }
+    else{
+        logger.info { "Book Not Available" }
+        return
+    }
+    logger.info { "Name of Borrower: " }
+    var borrowersName:String? = readLine().toString()
+    logger.info{"Days Borrowed: "}
+    var daysBorrowed: String? = readLine().toString()
+
+    hashBorrower.put("Name of Borrower","$borrowersName")
+    hashBorrower.put("Book Borrowed","$enteredBook")
+    hashBorrower.put("Days to be Borrowed","$daysBorrowed")
+
+    for (key in hashBorrower.keys){
+        logger.info{"$key = ${hashBorrower[key]}"}
+    }
+
+    logger.info { "Books Borrowed: $booksBorrowed" }
+
+
+
+
+}
