c89608074c6ab909777348c428398a1320716cff
Vismar Jay Alfaro <zyrenmisaki@gmail.com>
2022-10-24 20:25:30 +0800

    implemented data structures
    activity_01_c

--- src/main/kotlin/activity_01_c/Grocery.kt
+++ src/main/kotlin/activity_01_c/Grocery.kt
@@ -3,6 +3,15 @@ package activity_01_c
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
+
+//You are tasked to automate an inventory system for a grocery.
+//Identify the items that can be bought in a grocery store.
+//After listing the different items, identify the characteristics of the items.
+//You are also tasked to group the items in categories to help manage the grocery.
+// Use the proper data types.
 fun main() {
 
     //Items in the grocery
@@ -25,6 +34,11 @@ fun main() {
 
     var weight: Double?= null
     var isbn: Int?=null
+
+    //Data Structure
+    var hashItems:ArrayList<String> = ArrayList<String>()
+    var hashPrice:HashSet<String> = HashSet<String>()
+    var newPrice:HashSet<Double> = HashSet<Double>()
 
 
 }
